6bbab229f546bd4c784942c71fbafb68b4ae8831
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-09-23 14:34:42 -0400

    styles about page

--- components/Cart.js
+++ components/Cart.js
@@ -1,7 +1,49 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import products from '../shared/products';
 
 export default Cart = props => {
+    let getProductsInfoById = () => {  
+        let cartItemsId = props.screenProps.cartItemsIds;
+        let customerProductsInfo = cartItemsId.map( (singleId) => {
+            let item = products.filter( product => product.id === singleId)
+            return item[0];
+        });
+        return customerProductsInfo;
+    };
+
+    let showCartProducts = () => {
+        let cartProducts = getProductsInfoById();
+        let total = 0.00;
+        //calculate total and make each product jsx display
+        let cartProductsDisplay = cartProducts.map( product => {
+            total += product.price;
+            return(
+                <Row className={styles.borderBottom}>
+                    <Col md="8"><p><FontAwesomeIcon icon={faTimes} className={styles.removeCartItem + " mr-3"} aria-hidden="true" onClick={() => props.removeCartItem(product.id)}/>{product.name}</p></Col>
+                    <Col md="4" className={styles.price + "text-right"}><p> Price: ${product.price}</p></Col>
+                </Row>
+            );
+        });
+        let cartDisplay = <Row className="mt-5"><p>Looks like your cart is empty. Please add some awesome stuff to the cart to proceed. </p></Row>;
+        if(cartProductsDisplay.length){
+         cartDisplay = (
+            <Fragment>
+                <Button className="btn col-3 offset-9 mb-5" onClick={() => props.clearCart()}>Clear Cart</Button>
+                {cartProductsDisplay}            
+                <Row className="mt-5" ><Col className="text-right"><p> Total: ${total.toFixed(2)} </p></Col></Row>
+                <Row className="mt-5" ><Col className="text-right"><p> Total After Tax(7%): ${( total * 1.07 ).toFixed( 2 )}</p></Col></Row>
+                <CheckoutForm />        
+            </Fragment>
+        );
+        }
+        return (
+            <Fragment>
+                {cartDisplay}
+            </Fragment>
+        ); 
+    };
+
     return(
         <View>
             <Text>Cart page</Text>
--- components/about.js
+++ components/about.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Text, ScrollView } from 'react-native';
-import { Image } from 'react-native-elements';
+import { Text, ScrollView, Image, Dimensions } from 'react-native';
 
 export default About = props => {
+    let windowWidth = Dimensions.get('window').width;
     return (
-        <ScrollView style={{ flex: 1}} >
-            <Text >About page</Text>
-            <Image
-                source={ require('../assets/images/house.png') } 
-                style={{height: 300, width: 300}}  
-            />
-            <Text>
+        <ScrollView style={{ flex: 1, backgroundColor: '#FFF'}} >   
+            <Text style={{color: '#ea5e23', fontSize: 30, marginTop: 20, textAlign: 'center'}}>Who are we?</Text>
+            <Image source={ require('../assets/images/house.png') } style={{flex: 1, width: windowWidth, margin: 20}} resizeMode="contain"/>
+            <Text style={{fontSize: 18, lineHeight: 30, margin: 30}}>
                 lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
             </Text>
-            <Text>
+            <Text style={{fontSize: 18, lineHeight: 30, margin: 30}}>
                 lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus et voluptatibus, animi atque voluptatem temporibus ipsum aliquid pariatur porro quaerat tempora neque cumque veniam labore mollitia in sit molestiae magnam!
             </Text>
         </ScrollView>
